91f31f266dcf8057b30296fc3221c2c28bea4dd5
unknown <soleil-2005@mail.ru>
2021-05-13 15:18:27 +0300

    Add the JS

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
 const navToggle = document.querySelector('.nav-toggle');
 
-navToggle.addEventListener('click');
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
